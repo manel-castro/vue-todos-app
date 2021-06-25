bb4043f03e7152e3c0976015a2a4557b82607f4b
Manel <manel.castro39@gmail.com>
2021-06-25 20:13:23 +0200

    working with Vue!

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button action="toggle-add-task" text="Add task" color="green"/>
+    <Button text="Update task" color="blue"/>
+    <Button text="Delete task" color="red"/>
+
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  }
+}
+  
+</script>
+
+<style scoped> /*anything inhere will be only fo rthis component*/
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="(task) in tasks" :key="task.id">
+    <Task @toggle-reminder="id => $emit('toggle-reminder', id)" @delete-task="id => $emit('delete-task',id)" :task="task" />
+  </div>
+</template>
+
+<script>
+  import Task from './Task.vue'
+
+  export default {
+    name: 'Tasks',
+    props: {
+      tasks: Array,
+    },
+    components: {
+      Task,
+    },
+    emits: ['delete-task', 'toggle-reminder']
+  }
+</script>
